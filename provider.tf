c6e78fafde1e44f62a0281e1b719cfbd18ec1efd
Nikola-alpha <yekhaung131313@gmail.com>
2024-08-14 13:37:20 +0630

    remove backend region variable

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   backend "s3" {
   bucket         = "my-tf-state-bucket"
   key            = "tf-state"
-  region         = var.region
+  region         = "ap-southeast-1"
   dynamodb_table = "tf_lock_table"
 }
 }
@@ -15,7 +15,7 @@ terraform {
 
 
 provider "aws" {
-  region     = "ap-southeast-1"
+  region     = "var.region"
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
