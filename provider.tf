a53a6a880231e89bfa70fe67f402edb3628adb6f
Nikola-alpha <yekhaung131313@gmail.com>
2024-08-16 10:47:12 +0630

    testing github action with secret

--- provider.tf
+++ provider.tf
@@ -15,6 +15,6 @@ terraform {
 
 provider "aws" {
   region     = "ap-southeast-1"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  # access_key = var.AWS_ACCESS_KEY_ID
+  # secret_key = var.AWS_SECRET_ACCESS_KEY
 }
