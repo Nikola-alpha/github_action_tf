29c57fd57f447fdddb6323ed9129a02c4e116391
Nikola-alpha <yekhaung2224@gmail.com>
2024-08-28 21:18:08 +0630

    adding default tags

--- provider.tf
+++ provider.tf
@@ -17,4 +17,11 @@ provider "aws" {
   region     = "ap-southeast-1"
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
+  default_tags {
+    tags = {
+      Managed_by = "Terraform"
+      Environment = "pipeline"
+    }
+    
+  }
 }
